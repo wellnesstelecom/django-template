23f5f41392df7f324d9c3533dffe7c1a1edbf1b7
javisantana <jsantana@wtelecom.es>
2011-01-17 15:41:12 +0100

    base template

---
+++ django-template/settings.py
@@ -0,0 +1,119 @@
+# -*- encoding: utf-8 -*-
+
+import os 
+import logging
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+CACHE_BACKEND = 'dummy://'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database.sqlite',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'Europe/Madrid'
+LANGUAGE_CODE = 'es-ES'
+SITE_ID = 1
+
+USE_I18N = True
+USE_L10N = True
+
+PROJECT_ROOT = os.path.dirname(__file__)
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'j3v+(m*7wb^kbomqz&ask!)7k^%)=e8x9si3s2e-ywg6zonf(u'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+  os.path.join(PROJECT_ROOT, "templates"),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'south',
+    # sentry
+    'indexer',
+    'paging',
+    'sentry',
+    'sentry.client',
+
+    'debug_toolbar',
+)
+
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+INTERCEPT_REDIRECTS = False
+
+# sentry conf
+from sentry.client.handlers import SentryHandler
+logger = logging.getLogger()
+# ensure we havent already registered the handler
+if SentryHandler not in map(lambda x: x.__class__, logger.handlers):
+    logger.addHandler(SentryHandler())
+
+    # Add StreamHandler to sentry's default so you can catch missed exceptions
+    logger = logging.getLogger('sentry.errors')
+    logger.propagate = False
+    logger.addHandler(logging.StreamHandler())
+
+##########################################
+# this code must be at the end of this file
+##########################################
+
+try:
+   from local_settings import *
+except ImportError:
+   pass
---
+++ django-template/urls.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+
+from django.views.generic.simple import direct_to_template
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^sentry/', include('sentry.urls')),
+
+
+    url(r'^test/$', direct_to_template,
+        { 'template': 'test.html' },
+        name='test'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('', 
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                {'document_root': settings.MEDIA_ROOT}),
+    )

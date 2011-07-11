3bcc2e92383cc8d37660b6a6b2682ce8dbbbfc54
David Medina <davidmedina9@gmail.com>
2011-07-11 17:55:08 +0200

    new philosofy

--- django-template/local_settings.py
+++ django-template/local_settings.py
@@ -1,7 +1,17 @@
 COMPRESS = True
-COMPRESS_PRECOMPILERS = (
-    #('text/coffeescript', 'coffee --compile --stdio'),
-    #('text/less', 'lessc {infile} {outfile}'),
-    ('text/sass', 'sass {infile} {outfile}'),
-    #('text/x-scss', 'sass --scss {infile} {outfile}'),
-)
+#COMPRESS_PRECOMPILERS = (
+#    ('text/coffeescript', 'coffee --compile --stdio'),
+#    ('text/less', 'lessc {infile} {outfile}'),
+#    ('text/sass', 'sass {infile} {outfile}'),
+#    ('text/x-scss', 'sass --scss {infile} {outfile}'),
+#)
+#COMPRESS_CSS_FILTERS = [
+#    'compressor.filters.compass.CompassFilter',
+#    'compressor.filters.css_default.CssAbsoluteFilter',
+#]
+CACHES = {
+    'default': {
+    'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+    'LOCATION': '/var/tmp/django_cache',
+    }
+}
